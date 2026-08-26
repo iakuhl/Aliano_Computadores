9698f42ae4912884105b73afd070297030af4126
iakuhl <ianodark@gmail.com>
2026-08-25 23:26:11 -0300

    Add banner image to Banner component

--- Aliano_adaptado/src/componentes/Banner/Banner.jsx
+++ Aliano_adaptado/src/componentes/Banner/Banner.jsx
@@ -1,8 +1,12 @@
 import "./Banner.css";
+import bannerImagem from "./banner.png";
+
 
 export default function Banner() {
   return (
+    
     <header className="banner">
+      <img className="banner-imagem" src={bannerImagem} alt="Banner Aliano Computadores" />
       <p>ALIANO COMPUTADORES</p>
       <h1>Cadastro de produtos de informática</h1>
     </header>
